3658b5c798ddae0e5e30566c96ad8113ca1f75db
Andrew Mendoza <aalmendoza@ucdavis.edu>
2017-01-19 19:43:39 -0800

    Fix indentation

--- source/evaluate.py
+++ source/evaluate.py
@@ -14,61 +14,54 @@ from model import Model
 from six import text_type
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--save_dir', type=str, default='save',
-                       help='model directory to store checkpointed models')
-    parser.add_argument('--code_file', type=str, default='code.c',
-                       help='code file to evaluate')
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--save_dir', type=str, default='save',
+					   help='model directory to store checkpointed models')
+	parser.add_argument('--code_file', type=str, default='code.c',
+					   help='code file to evaluate')
 
-    args = parser.parse_args()
-    evaluate(args)
+	args = parser.parse_args()
+	evaluate(args)
 
 def evaluate(args):
-    with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
-        saved_args = cPickle.load(f)
-    with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'rb') as f:
-        chars, vocab = cPickle.load(f)
-    model = Model(saved_args, True)
-    with tf.Session() as sess:
-        tf.initialize_all_variables().run()
-        saver = tf.train.Saver(tf.all_variables())
-        ckpt = tf.train.get_checkpoint_state(args.save_dir)
-        if ckpt and ckpt.model_checkpoint_path:
-            saver.restore(sess, ckpt.model_checkpoint_path)
-            token_list = get_tokens(args.code_file)
-            vocab_token_list = convert_to_vocab_tokens(vocab, token_list, model.start_token,
-                model.eof_token, model.unk_token)
-            probs = model.evaluate(sess, chars, vocab, vocab_token_list)
-            display_results(token_list, probs)
-            print(calc_entropy(probs))
+	with open(os.path.join(args.save_dir, 'config.pkl'), 'rb') as f:
+		saved_args = cPickle.load(f)
+	with open(os.path.join(args.save_dir, 'chars_vocab.pkl'), 'rb') as f:
+		chars, vocab = cPickle.load(f)
+	model = Model(saved_args, True)
+	with tf.Session() as sess:
+		tf.initialize_all_variables().run()
+		saver = tf.train.Saver(tf.all_variables())
+		ckpt = tf.train.get_checkpoint_state(args.save_dir)
+		if ckpt and ckpt.model_checkpoint_path:
+			saver.restore(sess, ckpt.model_checkpoint_path)
+			token_list = get_tokens(args.code_file)
+			vocab_token_list = convert_to_vocab_tokens(vocab, token_list, model.start_token,
+				model.eof_token, model.unk_token)
+			probs = model.evaluate(sess, chars, vocab, vocab_token_list)
+			display_results(token_list, probs)
 
 def get_tokens(code_file):
-    tmp_outfile = '/tmp/out.txt'
-    simplePyLex.main(code_file, tmp_outfile, 3, "full", "True", "False")
-    with open(tmp_outfile) as f:
-        token_list = f.read().split(" ")
-    return token_list[:-1] # Remove ending newline entry
+	tmp_outfile = '/tmp/out.txt'
+	simplePyLex.main(code_file, tmp_outfile, 3, "full", "True", "False")
+	with open(tmp_outfile) as f:
+		token_list = f.read().split(" ")
+	return token_list[:-1] # Remove ending newline entry
 
 def convert_to_vocab_tokens(vocab, token_list, start_token, eof_token, unk_token):
-    res = [start_token]
-    for token in token_list:
-        if token in vocab:
-            res.append(token)
-        else:
-            res.append(unk_token)
+	res = [start_token]
+	for token in token_list:
+		if token in vocab:
+			res.append(token)
+		else:
+			res.append(unk_token)
 
-    res.append(eof_token)
-    return res
+	res.append(eof_token)
+	return res
 
 def display_results(token_list, probs):
-    for i in range(len(token_list)):
-        print("{0} => {1}".format(token_list[i], probs[i]))
-
-def calc_entropy(probs):
-    entropy = 0
-    for p in probs:
-        entropy += p * np.log2(p)
-    return entropy * -1
+	for i in range(len(token_list)):
+		print("{0} => {1}".format(token_list[i], probs[i]))
 
 if __name__ == '__main__':
-    main()
+	main()
--- source/model.py
+++ source/model.py
@@ -5,112 +5,112 @@ from tensorflow.python.ops import seq2seq
 import numpy as np
 
 class Model():
-    def __init__(self, args, infer=False):
-        self.start_token = '<START>'
-        self.eof_token = '<EOF>'
-        self.unk_token = '<UNK>'
+	def __init__(self, args, infer=False):
+	self.start_token = '<START>'
+	self.eof_token = '<EOF>'
+	self.unk_token = '<UNK>'
 
-        self.args = args
-        if infer:
-            args.batch_size = 1
-            args.seq_length = 1
+		self.args = args
+		if infer:
+			args.batch_size = 1
+			args.seq_length = 1
 
-        if args.model == 'rnn':
-            cell_fn = rnn_cell.BasicRNNCell
-        elif args.model == 'gru':
-            cell_fn = rnn_cell.GRUCell
-        elif args.model == 'lstm':
-            cell_fn = rnn_cell.BasicLSTMCell
-        else:
-            raise Exception("model type not supported: {}".format(args.model))
+		if args.model == 'rnn':
+			cell_fn = rnn_cell.BasicRNNCell
+		elif args.model == 'gru':
+			cell_fn = rnn_cell.GRUCell
+		elif args.model == 'lstm':
+			cell_fn = rnn_cell.BasicLSTMCell
+		else:
+			raise Exception("model type not supported: {}".format(args.model))
 
-        cell = cell_fn(args.rnn_size, state_is_tuple=True)
+		cell = cell_fn(args.rnn_size, state_is_tuple=True)
 
-        self.cell = cell = rnn_cell.MultiRNNCell([cell] * args.num_layers, state_is_tuple=True)
+		self.cell = cell = rnn_cell.MultiRNNCell([cell] * args.num_layers, state_is_tuple=True)
 
-        self.input_data = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
-        self.targets = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
-        self.initial_state = cell.zero_state(args.batch_size, tf.float32)
+		self.input_data = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
+		self.targets = tf.placeholder(tf.int32, [args.batch_size, args.seq_length])
+		self.initial_state = cell.zero_state(args.batch_size, tf.float32)
 
-        with tf.variable_scope('rnnlm'):
-            softmax_w = tf.get_variable("softmax_w", [args.rnn_size, args.vocab_size])
-            softmax_b = tf.get_variable("softmax_b", [args.vocab_size])
-            with tf.device("/cpu:0"):
-                embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
-                inputs = tf.split(1, args.seq_length, tf.nn.embedding_lookup(embedding, self.input_data))
-                inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
+		with tf.variable_scope('rnnlm'):
+			softmax_w = tf.get_variable("softmax_w", [args.rnn_size, args.vocab_size])
+			softmax_b = tf.get_variable("softmax_b", [args.vocab_size])
+			with tf.device("/cpu:0"):
+				embedding = tf.get_variable("embedding", [args.vocab_size, args.rnn_size])
+				inputs = tf.split(1, args.seq_length, tf.nn.embedding_lookup(embedding, self.input_data))
+				inputs = [tf.squeeze(input_, [1]) for input_ in inputs]
 
-        def loop(prev, _):
-            prev = tf.matmul(prev, softmax_w) + softmax_b
-            prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
-            return tf.nn.embedding_lookup(embedding, prev_symbol)
+		def loop(prev, _):
+			prev = tf.matmul(prev, softmax_w) + softmax_b
+			prev_symbol = tf.stop_gradient(tf.argmax(prev, 1))
+			return tf.nn.embedding_lookup(embedding, prev_symbol)
 
-        outputs, last_state = seq2seq.rnn_decoder(inputs, self.initial_state, cell, loop_function=loop if infer else None, scope='rnnlm')
-        output = tf.reshape(tf.concat(1, outputs), [-1, args.rnn_size])
-        self.logits = tf.matmul(output, softmax_w) + softmax_b
-        self.probs = tf.nn.softmax(self.logits)
-        loss = seq2seq.sequence_loss_by_example([self.logits],
-                [tf.reshape(self.targets, [-1])],
-                [tf.ones([args.batch_size * args.seq_length])],
-                args.vocab_size)
-        self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length
-        self.final_state = last_state
-        self.lr = tf.Variable(0.0, trainable=False)
-        tvars = tf.trainable_variables()
-        grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),
-                args.grad_clip)
-        optimizer = tf.train.AdamOptimizer(self.lr)
-        self.train_op = optimizer.apply_gradients(zip(grads, tvars))
-
-
-    def sample(self, sess, chars, vocab, sampling_type=1):
-        state = sess.run(self.cell.zero_state(1, tf.float32))
-        cur_tok = self.start_token
-        ret = ""
-
-        def weighted_pick(weights):
-            t = np.cumsum(weights)
-            s = np.sum(weights)
-            return(int(np.searchsorted(t, np.random.rand(1)*s)))
-
-        while True:
-            x = np.zeros((1, 1))
-            x[0, 0] = vocab[cur_tok]
-            feed = {self.input_data: x, self.initial_state:state}
-            [probs, state] = sess.run([self.probs, self.final_state], feed)
-            p = probs[0]
-
-            if sampling_type == 0:
-                sample = np.argmax(p)
-            elif sampling_type == 2:
-                if char == ' ':
-                    sample = weighted_pick(p)
-                else:
-                    sample = np.argmax(p)
-            else: # sampling_type == 1 default:
-                sample = weighted_pick(p)
-
-            pred = chars[sample]
-            if pred == self.eof_token:
-            	break
-
-            ret += pred + " "
-            cur_tok = pred
-        return ret
+		outputs, last_state = seq2seq.rnn_decoder(inputs, self.initial_state, cell, loop_function=loop if infer else None, scope='rnnlm')
+		output = tf.reshape(tf.concat(1, outputs), [-1, args.rnn_size])
+		self.logits = tf.matmul(output, softmax_w) + softmax_b
+		self.probs = tf.nn.softmax(self.logits)
+		loss = seq2seq.sequence_loss_by_example([self.logits],
+			[tf.reshape(self.targets, [-1])],
+			[tf.ones([args.batch_size * args.seq_length])],
+			args.vocab_size)
+		self.cost = tf.reduce_sum(loss) / args.batch_size / args.seq_length
+		self.final_state = last_state
+		self.lr = tf.Variable(0.0, trainable=False)
+		tvars = tf.trainable_variables()
+		grads, _ = tf.clip_by_global_norm(tf.gradients(self.cost, tvars),
+			args.grad_clip)
+		optimizer = tf.train.AdamOptimizer(self.lr)
+		self.train_op = optimizer.apply_gradients(zip(grads, tvars))
 
 
-    def evaluate(self, sess, chars, vocab, token_list):
-        token_probs = []
-    	state = sess.run(self.cell.zero_state(1, tf.float32))
-    	for n in range(len(token_list)-1):
-    		x = np.zeros((1, 1))
-    		x[0, 0] = vocab[token_list[n]]
-    		feed = {self.input_data: x, self.initial_state:state}
-    		[probs, state] = sess.run([self.probs, self.final_state], feed)
-    		prob_dist = probs[0]
-    		prob_next_token = prob_dist[vocab[token_list[n+1]]]
-    		token_probs.append(prob_next_token)
-    		print("Current token: {0}".format(token_list[n]))
-    		print("Next token: {0}, Prob: {1}".format(token_list[n+1], prob_next_token))
-    		print("Predicted next token: {0}, Prob: {1}\n".format(chars[np.argmax(prob_dist)], np.max(prob_dist)))
-    	return token_probs[:-1]
+	def sample(self, sess, chars, vocab, sampling_type=1):
+		state = sess.run(self.cell.zero_state(1, tf.float32))
+		cur_tok = self.start_token
+		ret = ""
+
+		def weighted_pick(weights):
+			t = np.cumsum(weights)
+			s = np.sum(weights)
+			return(int(np.searchsorted(t, np.random.rand(1)*s)))
+
+		while True:
+			x = np.zeros((1, 1))
+			x[0, 0] = vocab[cur_tok]
+			feed = {self.input_data: x, self.initial_state:state}
+			[probs, state] = sess.run([self.probs, self.final_state], feed)
+			p = probs[0]
+
+			if sampling_type == 0:
+				sample = np.argmax(p)
+			elif sampling_type == 2:
+				if char == ' ':
+					sample = weighted_pick(p)
+				else:
+					sample = np.argmax(p)
+			else: # sampling_type == 1 default:
+				sample = weighted_pick(p)
+
+			pred = chars[sample]
+			if pred == self.eof_token:
+				break
+
+			ret += pred + " "
+			cur_tok = pred
+		return ret
+
+
+	def evaluate(self, sess, chars, vocab, token_list):
+		token_probs = []
+		state = sess.run(self.cell.zero_state(1, tf.float32))
+		for n in range(len(token_list)-1):
+			x = np.zeros((1, 1))
+			x[0, 0] = vocab[token_list[n]]
+			feed = {self.input_data: x, self.initial_state:state}
+			[probs, state] = sess.run([self.probs, self.final_state], feed)
+			prob_dist = probs[0]
+			prob_next_token = prob_dist[vocab[token_list[n+1]]]
+			token_probs.append(prob_next_token)
+			print("Current token: {0}".format(token_list[n]))
+			print("Next token: {0}, Prob: {1}".format(token_list[n+1], prob_next_token))
+			print("Predicted next token: {0}, Prob: {1}\n".format(chars[np.argmax(prob_dist)], np.max(prob_dist)))
+		return token_probs[:-1]
