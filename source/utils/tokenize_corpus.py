7922287edcefd50d7eafdb08399b243664aa16bf
Andrew Mendoza <aalmendoza@ucdavis.edu>
2017-04-21 11:26:05 -0700

    Add check to not include empty files

--- source/utils/tokenize_corpus.py
+++ source/utils/tokenize_corpus.py
@@ -31,15 +31,16 @@ def tokenize_corpus(corpus_dir, corpus_ext, out_dir):
 	i = 0
 	for file in corpus_files:
 		tokenized_text, token_types = simplePyLex.tokenize_file(file, 3)
-		tokenized_file = os.path.join(out_dir, "{0}{1}".format(i, corpus_ext))
-		token_types_file = os.path.join(out_dir, "{0}{1}.types.pkl".format(i, corpus_ext))
-		with open(tokenized_file, 'w') as f:
-			f.write(tokenized_text)
-		with open(token_types_file, 'wb') as f:
-			cPickle.dump(token_types, f)
+		if len(tokenized_text) != 0:
+			tokenized_file = os.path.join(out_dir, "{0}{1}".format(i, corpus_ext))
+			token_types_file = os.path.join(out_dir, "{0}{1}.types.pkl".format(i, corpus_ext))
+			with open(tokenized_file, 'w') as f:
+				f.write(tokenized_text)
+			with open(token_types_file, 'wb') as f:
+				cPickle.dump(token_types, f)
 
-		print("{0} -> {1}".format(file, tokenized_file))
-		i += 1
+			print("{0} -> {1}".format(file, tokenized_file))
+			i += 1
 
 if __name__ == '__main__':
 	main()
